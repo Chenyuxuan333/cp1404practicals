c48a338ea648d747600490a10d020a3c20eb43ed
25146 <yuxuan.chen3@my.jcu.edu.au>
2025-09-24 13:56:11 +0800

    Move all the code into main function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,8 +1,10 @@
 MIN_LENGTH = 9
 
-pass_word = input("Enter your password: ")
-while len(pass_word) < MIN_LENGTH:
-    print("Invalid password!!")
+def main():
     pass_word = input("Enter your password: ")
-else:
-    print("*" * len(pass_word))
+    while len(pass_word) < MIN_LENGTH:
+        print("Invalid password!!")
+        pass_word = input("Enter your password: ")
+    else:
+        print("*" * len(pass_word))
+main()
