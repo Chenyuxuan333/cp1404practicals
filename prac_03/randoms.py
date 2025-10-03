4f036e75398d8c5e3993a79a04bed749c1a86e5c
Administrator <yuxuan.chen3@my.jcu.edu.au>
2025-10-03 16:16:32 +0800

    write the code to produce a random number

---
+++ prac_03/randoms.py
@@ -0,0 +1,7 @@
+# i see 12 on line 1, the smallest is 5 , largest is 20
+# I SEE 3 on line 2, the smallest is 3, largest is 9 , can not produce a 4
+# i see 5.212536220745395 on line 3 , the smallest is 2.5, largest is 5.5
+
+import random
+
+print(random.randint(1,100))
