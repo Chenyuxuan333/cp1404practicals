52631c4e397b60999fb335930ae231cdc1f00ff4
Administrator <yuxuan.chen3@my.jcu.edu.au>
2025-10-12 14:16:08 +0800

    COMPLETE list warmup

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,23 @@
+numbers = ["Ten", 1, 4, 1, 5, 9, 1]
+print(numbers[0])
+#The output is 3
+print(numbers[-1])
+#The output is 2
+print(numbers[3])
+#The output is 1
+print(numbers[:-1])
+#The output is [3, 1, 4, 1, 5, 9]
+print(numbers[3:4])
+#The output is [1]
+print(5 in numbers)
+#The output is True
+print(7 in numbers)
+#The output is False
+print("3" in numbers)
+#The output is False
+print(numbers + [6, 5, 3])
+#The output is [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+print(numbers[2:])
+
+print(9 in numbers)
