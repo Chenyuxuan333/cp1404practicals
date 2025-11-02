df10684da909fc21c124f2ab64e254948c796366
Chenyuxuan333 <yuxuan.chen3@my.jcu.edu.au>
2025-11-02 15:36:34 +0800

    modify two flies

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -1,18 +1,25 @@
-"""
-CP1404/CP5632 Practical - Client code to use the Car class.
-Note that the import has a folder (module) in it.
-This is why we name our folders with no spaces or capitals, as valid module names.
-"""
+"""CP1404/CP5632 Practical - Client code to use the Car class."""
 
 from prac_06.car import Car
 
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("My Car", 180)
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
+
+    # 1. Create limo with 100 fuel
+    limo = Car("Limo", 100)
+    # 2. Add 20 fuel to limo
+    limo.add_fuel(20)
+    # 3. Print limo's fuel
+    print(f"Limo has fuel: {limo.fuel}")
+    # 4. Attempt to drive 115 km
+    limo.drive(115)
+    # 7. Print limo to check __str__
+    print(limo)
 
 
 main()
